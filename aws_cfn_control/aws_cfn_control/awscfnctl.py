06a61600fa48e717b3064db9411e8b4c9ff0e43f
duff <duff@amazon.com>
2018-03-01 07:42:39 -0800

    adding copyright info

--- aws_cfn_control/aws_cfn_control/awscfnctl.py
+++ aws_cfn_control/aws_cfn_control/awscfnctl.py
@@ -1,4 +1,16 @@
-#!/Users/duff/Envs/boto3-144/bin/python
+#
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under the License.
+#
+
 
 import os
 import sys
@@ -854,7 +866,7 @@ class CfnControl:
                             self.rm_cfnconfig_file(cfn_config_file)
                             return
                         print('EC2 keys found in {0}:'.format(self.region))
-                        ##print('  {0}'.format(', '.join(self.key_pairs)))
+                        #print('  {0}'.format(', '.join(self.key_pairs)))
                         for k in self.key_pairs:
                             print('  {0}'.format(k))
                         cli_val = raw_input("Select EC2 Key: ")
