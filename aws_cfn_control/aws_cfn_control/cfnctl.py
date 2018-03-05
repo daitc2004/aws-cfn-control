e206cad1d93c934460b684d5db5c42997f68eead
duff <duff@amazon.com>
2018-03-04 16:10:50 -0800

    added default functionality to cfnctl, will show all stacks that are not "DELETE_COMPLETE"

--- aws_cfn_control/aws_cfn_control/cfnctl.py
+++ aws_cfn_control/aws_cfn_control/cfnctl.py
@@ -24,10 +24,10 @@ def arg_parse():
     parser = argparse.ArgumentParser(prog=progname, description='Launch a stack, with a config file')
 
     opt_group = parser.add_argument_group()
-    opt_group.add_argument('-s', dest='stack_name', required=False)
+    opt_group.add_argument('-c', dest='stack_name', required=False)
     opt_group.add_argument('-r', dest='region', required=False)
-    opt_group.add_argument('-c', dest='config_file', required=False)
-    opt_group.add_argument('-b', dest='template_url', required=False, help='Build config file from template URL')
+    opt_group.add_argument('-f', dest='config_file', required=False)
+    opt_group.add_argument('-t', dest='template_url', required=False, help='Build config file from template URL')
     opt_group.add_argument('-n', dest='rollback', required=False, help='Do not rollback', action='store_true')
     opt_group.add_argument('-v', dest='verbose_config_file', required=False, action='store_true', help='Verbose config file')
     opt_group.add_argument('-p', dest='aws_profile', required=False, help='AWS Profile')
@@ -57,18 +57,34 @@ def main():
         aws_profile = args.aws_profile
         print('Using profile {0}'.format(aws_profile))
 
-    client = CfnControl(region=region,aws_profile=aws_profile)
-
-    if template:
-        client.build_cfn_config(args.template_url, verbose=verbose_config_file)
-        return rc
-
     if args.rollback:
         rollback = 'DO_NOTHING'
 
-    client.cr_stack(stack_name, config_file, set_rollback=rollback)
+    client = CfnControl(region=region,aws_profile=aws_profile)
 
-    return rc
+    if template and config_file:
+        errmsg = "Specify either the CFN template location, or the local config file to use"
+        raise ValueError(errmsg)
+    elif template:
+        try:
+            response = client.build_cfn_config(args.template_url, verbose=verbose_config_file)
+            return response
+        except Exception as e:
+            raise ValueError(e)
+    elif config_file:
+        try:
+            response = client.cr_stack(stack_name, config_file, set_rollback=rollback)
+            return response
+        except Exception as e:
+            raise ValueError(e)
+    else:
+        try:
+            print("Gathering info on CFN stacks...")
+            response = client.ls_stacks(show_deleted=False)
+            return response
+        except Exception as e:
+            raise ValueError(e)
+
 
 if __name__ == "__main__":
     try:
