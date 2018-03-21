fb96827bf3e9be888b7551da2b77f02e8e66a07e
duff <duff@amazon.com>
2018-03-20 23:24:05 -0700

    added prompt for each cfn parameter, uses default if not given.

--- aws_cfn_control/aws_cfn_control/cfnctl.py
+++ aws_cfn_control/aws_cfn_control/cfnctl.py
@@ -29,7 +29,8 @@ def arg_parse():
     opt_group.add_argument('-a', dest='ls_all_stack_info', required=False, help='List more info on all stacks', action='store_true')
     opt_group.add_argument('-b', dest='bucket', required=False, help='Bucket to upload template to')
     opt_group.add_argument('-c', dest='create_stack', required=False, help="Create a stack", action='store_true')
-    opt_group.add_argument('-f', dest='config_file', required=False, help="cfnctl config file in ~/.cfnctlconfig")
+    opt_group.add_argument('-d', dest='del_stack', required=False, help="Delete a stack", action='store_true')
+    opt_group.add_argument('-f', dest='param_file', required=False, help="cfnctl stack parameter file (includes template)")
     opt_group.add_argument('-l', dest='ls_stacks', required=False, help='List stacks', action='store_true')
     opt_group.add_argument('-nr', dest='no_rollback', required=False, help='Do not rollback', action='store_true')
     opt_group.add_argument('-p', dest='aws_profile', required=False, help='AWS Profile')
@@ -51,14 +52,15 @@ def main():
     args = arg_parse()
     rollback = 'ROLLBACK'
 
-    region = args.region
-    create_stack = args.create_stack
     bucket = args.bucket
-    stack_name = args.stack_name
-    template = args.template
-    config_file = args.config_file
+    create_stack = args.create_stack
+    del_stack = args.del_stack
+    param_file = args.param_file
     ls_stacks = args.ls_stacks
     ls_all_stack_info = args.ls_all_stack_info
+    region = args.region
+    stack_name = args.stack_name
+    template = args.template
     ##verbose_config_file = args.verbose_config_file
 
     errmsg_cr = "Creating a stack requires create flag (-c), and both the stack name (-s), and for new stacks " \
@@ -94,7 +96,7 @@ def main():
     elif create_stack:
         if stack_name and not template:
             try:
-                response = client.cr_stack(stack_name, config_file, set_rollback=rollback)
+                response = client.cr_stack(stack_name, param_file, set_rollback=rollback)
             except Exception as e:
                 raise ValueError(e)
 
@@ -105,10 +107,10 @@ def main():
                     errmsg = 'File "{}" does not exists'.format(template)
                     raise ValueError(errmsg)
             try:
-                if config_file:
-                    config_file = config_file
-                    print("Should use {}".format(config_file))
-                response = client.cr_stack(stack_name, config_file, set_rollback=rollback, template=template)
+                if param_file:
+                    param_file = param_file
+                    print("Should use {}".format(param_file))
+                response = client.cr_stack(stack_name, param_file, set_rollback=rollback, template=template)
                 return
             except Exception as e:
                 if "Member must have length less than or equal to 51200" in e[0]:
@@ -116,7 +118,7 @@ def main():
                         print("Uploading {0} to bucket {1} and creating stack".format(template, bucket))
                         try:
                             template_url = client.upload_to_bucket(template,bucket,template)
-                            response = client.cr_stack(stack_name, config_file, set_rollback=rollback, template=template_url)
+                            response = client.cr_stack(stack_name, param_file, set_rollback=rollback, template=template_url)
                         except Exception as e:
                             raise ValueError(e)
                     else:
@@ -131,10 +133,15 @@ def main():
             raise ValueError(errmsg_cr)
         elif not template and not stack_name:
             raise ValueError(errmsg_cr)
-    elif config_file or stack_name:
+    elif del_stack:
+        if not stack_name:
+            errmsg = "Must specify a stack to delete (-s)"
+            raise ValueError(errmsg)
+        client.del_stack(stack_name)
+    elif param_file or stack_name:
         raise ValueError(errmsg_cr)
     else:
-        print("No actions requested")
+        print("No actions requested - shouldn't have got this far.")
         return 0
 
 
